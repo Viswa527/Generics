49a6691d49114880c3941f769705fcb48808c4f3
Viswa527 <Viswa527@github.com>
2020-05-01 11:43:35 +0530

    [Viswa]Refactor.String at different position in parameter returns Maximum length of string

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -4,11 +4,26 @@ import org.junit.Test;
 
 public class Testing {
 	@Test
+	public void givenThreeString_ComparedWithStringLengthObject_ReturnMaximumCharacterdString()
+	{
+		MaximumNumber maximumNumber=new MaximumNumber();
+		String result = maximumNumber.findMaximumLengthOfString("Bananna","Apple","Orange");
+		Assert.assertEquals("Bananna",result);
+	}
+	@Test
+	public void givenThreeString_ComparedWithStringLengthObject_ReturnsMaximumCharacteredString()
+	{
+		MaximumNumber maximumNumber=new MaximumNumber();
+		String result = maximumNumber.findMaximumLengthOfString("Bananna","PineApple","Apple");
+		Assert.assertEquals("PineApple",result);
+	}
+	@Test
 	public void givenThreeString_ComparedWithStringLengthObject_ReturnsMaximumCharacterdString()
 	{
 		MaximumNumber maximumNumber=new MaximumNumber();
-		String result = maximumNumber.findMaximumLengthOfString("Bananna","PineApple","CustaredApple");
+		String result = maximumNumber.findMaximumLengthOfString("Apple","PineApple","CustaredApple");
 		Assert.assertEquals("CustaredApple",result);
 	}
+
 
 }
