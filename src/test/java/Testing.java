811d2d79a7e17e10585c6c1261d1c16a9c633b9c
Viswa527 <Viswa527@github.com>
2020-05-01 10:48:30 +0530

    [Viswa]Added.Maximum number in 3st Postion returns Same number

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -7,8 +7,8 @@ public class Testing {
 	public void givenThreeNumbers_ComparedWithIntegerObject_ReturnsMaximumNumber()
 	{
 		MaximumNumber maxnumber = new MaximumNumber();
-		float number = maxnumber.findMaximumNumber(221.3f, 1244.4f, 114.7f);
-		Assert.assertEquals(1244.4f,number,0);
+		float number = maxnumber.findMaximumNumber(2211.3f, 1244.4f, 11492.7f);
+		Assert.assertEquals(11492.7f,number,0);
 	}
 	
 }
