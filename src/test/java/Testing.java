1353becd9722d4f32ca809e1de523d496f946f6c
Viswa527 <Viswa527@github.com>
2020-05-01 10:28:40 +0530

    [Viswa]Refactor.Maximum number in different position returns Max number

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -4,6 +4,20 @@ import org.junit.Test;
 
 public class Testing {
 	@Test
+	public void givenThreeNumbers_ComparedWithIntegerObject_ReturnsMaximumNumber()
+	{
+		MaximumNumber maxnumber = new MaximumNumber();
+		int number = maxnumber.findMaximumNumber(221, 124, 114);
+		Assert.assertEquals(221,number);
+	}
+	@Test
+	public void givenThreeNumbers_ComparedWithIntegerObjects_ReturnMaximumNumber()
+	{
+		MaximumNumber maxnumber = new MaximumNumber();
+		int number = maxnumber.findMaximumNumber(-10, 0, -14);
+		Assert.assertEquals(0,number);
+	}
+	@Test
 	public void givenThreeNumbers_ComparedWithIntegerObjects_ReturnsMaximumNumber()
 	{
 		MaximumNumber maxnumber = new MaximumNumber();
