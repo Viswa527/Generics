fb9860ab272e22769fb94a7fb8bffcc2598fdc98
Viswa527 <Viswa527@github.com>
2020-05-01 10:51:14 +0530

    [Viswa]Added.Maximum number in different Postion returns Maximum Float number

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -4,11 +4,23 @@ import org.junit.Test;
 
 public class Testing {
 	@Test
-	public void givenThreeNumbers_ComparedWithIntegerObject_ReturnsMaximumNumber()
+	public void givenThreeNumbers_ComparedWithFloatObject_ReturnsMaximumNumber()
 	{
 		MaximumNumber maxnumber = new MaximumNumber();
-		float number = maxnumber.findMaximumNumber(2211.3f, 1244.4f, 11492.7f);
-		Assert.assertEquals(11492.7f,number,0);
+		float number = maxnumber.findMaximumNumber(2211.3f, 1244.4f, 114.7f);
+		Assert.assertEquals(2211.3f,number,0);
 	}
-	
+	@Test
+	public void givenThreeNumbers_ComparedWithFloatObject_ReturnMaximumNumber()
+	{
+		MaximumNumber maxnumber = new MaximumNumber();
+		float number = maxnumber.findMaximumNumber(2211.3f, 124458.4f, 11492.7f);
+		Assert.assertEquals(124458.4f,number,0);
+	}
+	@Test
+	public void givenThreeNumber_ComparedWithFloatObject_ReturnsMaximumNumber() {
+		MaximumNumber maxnumber = new MaximumNumber();
+		float number = maxnumber.findMaximumNumber(2211.3f, 1244.4f, 11492.7f);
+		Assert.assertEquals(11492.7f, number, 0);
+	}
 }
